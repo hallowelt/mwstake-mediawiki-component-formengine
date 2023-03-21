1772ae884fa64b18cd364fad316565a2a96e0452
d.savuljesku <d.savuljesku@gmail.com>
2023-03-21 09:38:10 +0100

    Get rid of OOJSPlus dependency

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_FORMENGINE_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_FORMENGINE_VERSION', '2.0.5' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_FORMENGINE_VERSION', '2.0.6' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'formengine', function () {
@@ -130,8 +130,6 @@ MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 			'dependencies' => [
 				"ext.forms.define",
 				"oojs-ui",
-				"ext.oOJSPlus.data",
-				"ext.oOJSPlus.widgets",
 				"mediawiki.widgets.CategoryMultiselectWidget",
 				"mediawiki.widgets.DateInputWidget"
 			],
